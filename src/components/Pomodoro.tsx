dacb9c550a081e2122e4ff2513bae36ff903e547
Lyor Ouakil <lyor.ouakil@gmail.com>
2022-01-18 21:59:12 +0100

    create pomodoro tracker first step init

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Pomodoro: React.FC = () => (
-  <div>
-    <h1>Hello World second</h1>
-  </div>
-);
+const getCurrentTime = (seconds: number) => {
+  console.log('call');
+  return {
+    hours: Math.floor(seconds / (60 * 60)),
+    minutes: Math.floor(seconds / 60),
+    seconds: Math.ceil((seconds % (60 * 60)) % 60)
+  };
+};
+
+const Pomodoro: React.FC = () => {
+  const [time, setTimer] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 1500
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimer(getCurrentTime(time.seconds - 1));
+    }, 1000);
+  });
+
+  return (
+    <div>
+      <h1>Time: {time.seconds} </h1>
+    </div>
+  );
+};
 
 export default Pomodoro;
